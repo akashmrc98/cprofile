3f394087226a00ba474bec14881cf5e240cc4c21
akashmadduru <akashmadduru@gmail.com>
2025-05-19 07:07:48 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
 		const pdfUrl = "/resume.pdf";
 		const link = document.createElement("a");
 		link.href = pdfUrl;
-		link.download = "document.pdf"; // specify the filename
+		link.download = "akashmadduru.pdf"; // specify the filename
 		document.body.appendChild(link);
 		link.click();
 		document.body.removeChild(link);
